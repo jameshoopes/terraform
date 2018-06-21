8cf34918c227efdc4c8057166875678fa2dbccdf
James Hoopes <JHOOPES@pacsuncorp.com>
2018-06-21 15:08:50 -0700

    Add Security Group

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "jhtest" {
-ami				=	"ami-40d28157"
-instance_type	=	"t2.micro"
+ami						=	"ami-40d28157"
+instance_type			=	"t2.micro"
+vpc_security_group_ids	=	["${aws_security_group.jhtestsg.id}"]
+
+user_data		=	<<-EOF
+					#!/bin/bash
+					echo "Hello World!" > index.html
+					nohup busybox httpd -f -p 8080 &
+					EOF
 
 tags {
 	Name="Terraform-Example"
+	}
+}
+
+resource "aws_security_group" "jhtestsg" {
+	name	=	"terraform-example-instance"
+	
+	ingress {
+	from_port	=	8080
+	to_port		=	8080
+	protocol	=	"tcp"
+	cidr_blocks	=	["12.155.228.3/32"]
 	}
 }
