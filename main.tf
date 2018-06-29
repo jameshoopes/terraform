c0631d00d6cd53d57cb8bec9b2a336fe3668f931
James Hoopes <hoopes@hotmail.com>
2018-06-29 15:13:11 -0700

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,29 @@ provider "aws" {
 	region="us-east-1"
 }
 
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "jhlcfg" {
+	image_id		=	"ami-40d28157"
+	instance_type	=	"t2.micro"
+	security_groups	= ["${aws_security_group.instance.id}"]
+	
+	user_data		=	<<-EOF
+						#!/bin/bash
+						echo "Hello, World!" > index.html
+						nohup busybox httpd -f -port
+						${var.server_port}" &
+						EOF
+	lifecycle {
+		create_before_destroy	=	true
+	}
+}
+
+variable "server_port" {
+	description = "The port the server will use for HTTP requests"
+	default=8080
+}
+
 resource "aws_instance" "jhtest" {
 ami						=	"ami-40d28157"
 instance_type			=	"t2.micro"
@@ -10,7 +33,8 @@ vpc_security_group_ids	=	["${aws_security_group.jhtestsg.id}"]
 user_data		=	<<-EOF
 					#!/bin/bash
 					echo "Hello World!" > index.html
-					nohup busybox httpd -f -p 8080 &
+					nohup busybox httpd -f -p 
+					"${var.server_port}" &
 					EOF
 
 tags {
@@ -18,13 +42,40 @@ tags {
 	}
 }
 
+resource "aws_autoscaling_group" "jhautosc" {
+	launch_configuration	=	"${aws_launch_configuration.jhlcfg.id}"
+	availability_zones		=	["${data.aws_availability_zones.all.names}"]
+	
+	min_size	=	2
+	max_size	=	10
+	
+	tag {
+	key					=	"Name"
+	value				=	"terraform-asg-example"
+	propagate_at_launch	=	true
+	}
+}
+
+resource "aws_elb" "jhelb" {
+	name				=	"terraform-asg_example"
+	availability_zones	=	["${data.aws_availability_zones.all.names}"]
+}
+
 resource "aws_security_group" "jhtestsg" {
 	name	=	"terraform-example-instance"
 	
 	ingress {
-	from_port	=	8080
-	to_port		=	8080
+	from_port	=	"${var.server_port}"
+	to_port		=	"${var.server_port}"
 	protocol	=	"tcp"
 	cidr_blocks	=	["12.155.228.3/32"]
 	}
+	
+	lifecycle {
+		create_before_destroy	=	true
+	}
+}
+
+output "public_ip" {
+value="${aws_instance.jhtest.public_ip}"
 }
